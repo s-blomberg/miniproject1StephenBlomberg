18e1122c3dbd8b04650d5fa88f56d8d9479ffaf7
Stephen Blomberg <s_blomberg@mail.fhsu.edu>
2024-09-20 18:25:15 -0500

    Added graph title with ticker added x/y labels added price min/max -/+ 5%

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import pprint
 from datetime import datetime, timedelta
 import numpy as np
 import matplotlib.pyplot as plt
+import copy
 
 #Get today's date
 today = datetime.now()
@@ -24,13 +25,16 @@ for ticker in mytickers:
     result = yf.Ticker(ticker)
     hist = result.history(start=ten_days_ago, end=today)
     last10days = []
-    for date in hist['Close'][:10]:
+    for date in hist['Close'][:11]:
         last10days.append(date)
-    if len(last10days) == 10:
+    if len(last10days) >= 10:
         myarray = np.array(last10days)
+        max_price = myarray.max() + (myarray.max()*.05)
+        min_price = myarray.min() - (myarray.min()*.05)
         plt.plot(myarray)
         plt.xlabel('Days Ago')
         plt.ylabel('Closing Price')
+        plt.axis((9, 0, min_price, max_price))
         plt.title(f"{ticker} Last 10 Trading Days")
         plt.show()
     else:
